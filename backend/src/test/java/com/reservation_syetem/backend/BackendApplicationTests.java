03ba3c6d1c9c7c9e348973a4b8d2b046d5b0c388
Lilirtu <leacharafeddine@icloud.com>
2025-09-22 21:00:03 +0200

    Initalized backend with spring boot

---
+++ backend/src/test/java/com/reservation_syetem/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation_syetem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
